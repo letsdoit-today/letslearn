ffe0ad3dc9db176041d42d22221db3884e4a7689
letsdoit <mouse0407@163.com>
2025-12-28 09:41:09 +0800

    预先生成sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import sitemap from "vite-plugin-sitemap"
 
 export default defineConfig({
   base: '/', // 設置為根路徑，適用於GitHub Pages
   plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://learn.letsdoit.today',
-      dynamicRoutes: [
-        '/falling-ball/.',
-        '/friction-inclined-plane/.',
-        '/air-water-refraction/.',
-        '/convex-lens/.',
-        '/concave-lens/.',
-        '/eye-simulation/.'
-      ],
-      changefreq: 'daily',
-      priority: 0.8,
-      lastmod: new Date()
-    })
+    react()
   ],
   resolve: {
     alias: {
